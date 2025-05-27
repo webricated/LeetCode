8c488ed58103ada48efff7bc1e3c884992883632
MUKUND KUMAR <display.sharif@gmail.com>
2025-05-27 11:13:14 +0530

    Time: 274 ms (5.82%), Space: 40.3 MB (68.85%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
         if(num < 2) return true;
-        if(num < 101){
+        if(num < 401){
             for(int i = 2; i < (num/2)+1; i++){
             if(i*i == num) return true;
             }
         }
-        else if(num >= 101){
-            for(int i = 2; i < (num/10)+1; i++){
+        else if(num >= 401){
+            for(int i = 2; i < (num/28)+1; i++){
             if(i*i == num) return true;
             }
         }
