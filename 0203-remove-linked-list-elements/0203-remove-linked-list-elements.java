abdd77cbb1e31ce01c1382a298be8910fc1b99d9
MUKUND KUMAR <display.sharif@gmail.com>
2025-06-03 08:50:58 +0530

    Time: 0 ms (100%), Space: 45.5 MB (67.15%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -10,12 +10,14 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
+        // Because head is traversing/checking val/returning head together : Base Case
         if(head == null) return null;
         ListNode newList = removeElements(head.next, val);
-
+        // Skip this node if val matches
         if(head.val == val) return newList;
+        // Include in the list or do not skip if val doesn't matches
         else head.next = newList;
-
+        // Returning head because head is used to traverse here
         return head;
     }
 }
