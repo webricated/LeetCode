4512f7f6522762163452d3f63216fe8dab2e0948
MUKUND KUMAR <display.sharif@gmail.com>
2025-06-13 13:17:57 +0530

    Time: 0 ms (100%), Space: 42.2 MB (8.16%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,31 +1,31 @@
-// 0094-binary-tree-inorder-traversal.java
-// Morris Traversal for Inorder
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
 public class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        TreeNode curr = root;
-
-        while (curr != null) {
-            if (curr.left == null) {
-                result.add(curr.val);
-                curr = curr.right;
-            } else {
-                TreeNode predecessor = curr.left;
-                while (predecessor.right != null && predecessor.right != curr) {
-                    predecessor = predecessor.right;
-                }
-
-                if (predecessor.right == null) {
-                    predecessor.right = curr;
-                    curr = curr.left;
-                } else {
-                    predecessor.right = null;
-                    result.add(curr.val);
-                    curr = curr.right;
-                }
-            }
-        }
-
+        List<Integer> result = new LinkedList<>();
+        inorderHelper(root, result);
         return result;
+    }
+
+    private void inorderHelper(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+
+        inorderHelper(node.left, result);
+        result.add(node.val);
+        inorderHelper(node.right, result);
     }
 }
