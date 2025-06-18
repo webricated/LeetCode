f50ed4f90b1cc6bed6ba1eae0c33aeebaf830c51
MUKUND KUMAR <display.sharif@gmail.com>
2025-06-18 13:32:55 +0530

    Time: 0 ms (100%), Space: 46.2 MB (14.14%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,10 +1,44 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++) 
-            if(nums[i] == target)  
-                return i;
+        int start = 0, end = nums.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] < target) start = mid + 1;
+            else if(nums[mid] > target) end = mid - 1;
+            else return mid;
+        }
         return -1;
     }
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public static int search(int[] nums, int target) {
+//         int start = 0;
+//         int end = nums.length-1;
+
+//         while(start<=end){
+//             int mid = start + (end - start)/2;
+//             if (nums[mid]<target) {
+//                 start = mid +1;
+//             } else if (nums[mid]>target) {
+//                 end = mid-1;
+//             } else{
+//                 return mid;
+//             }
+//         }
+//         return -1;
+//     }
+// }
