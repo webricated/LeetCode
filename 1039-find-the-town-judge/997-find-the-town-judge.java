29d71af05489a2d29db7efb8436e7bcfb6214f1a
MUKUND KUMAR <display.sharif@gmail.com>
2025-07-26 12:24:58 +0530

    Create 997-find-the-town-judge.java

---
+++ 1039-find-the-town-judge/997-find-the-town-judge.java
@@ -0,0 +1,56 @@
+# 1. Adjacency List
+
+  ```Java
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        if(N <= 0) return -1;
+        if(N == 1) return 1;
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        for(int i = 0; i < N; i++) adj.add(new ArrayList<Integer>());
+
+        for(int i = 0; i < trust.length; i++){
+            int u = trust[i][0];
+            int v = trust[i][1];
+            adj.get(u-1).add(v-1);
+        }
+
+        for(int i = 0; i < adj.size(); i++){
+            boolean flag = false;
+            if(adj.get(i).size() == 0){
+                for(int j = 0; j < adj.size(); j++){
+                    if(i == j) continue;
+                    flag = adj.get(j).contains(i);
+                    if(flag == false) break;
+                }
+                if(flag == true) return i+1;
+            }
+        }
+        return -1;
+    }
+}
+
+
+  ```
+
+
+# 2. Iterative Method
+
+  ```Java
+  class Solution {
+    public int findJudge(int n, int[][] trust) {
+        if(trust.length == 0 && n ==1) return 1;
+        int[] count =new int[n+1];
+
+        for(int[] person : trust){
+            count[person[0]]--;
+            count[person[1]]++;
+        }
+
+        for(int person = 0; person < count.length; person++){
+            if(count[person] == n - 1) return person;
+        }
+        return -1;
+    }
+}
+```
