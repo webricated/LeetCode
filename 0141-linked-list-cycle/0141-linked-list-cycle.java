cc2453b35a276bb9613d6561cf3767c901371747
MUKUND KUMAR <display.sharif@gmail.com>
2025-08-05 15:34:13 +0530

    Time: 0 ms (100%), Space: 44.8 MB (27.34%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,15 +11,12 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null) return false;
         ListNode slow = head;
-        ListNode fast = head.next;
-
-        while(slow != fast){
-            if(fast.next == null || fast.next.next == null) return false;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
-        }
-        return true;
+            if(slow == fast) return true;
+        } return false;
     }
 }
