13900ecdb8dadd8bcc29a19d0e95958ad9fb3856
MUKUND KUMAR <display.sharif@gmail.com>
2025-08-16 16:58:41 +0530

    Time: 0 ms (100%), Space: 41.4 MB (16.33%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -4,3 +4,26 @@ class Solution {
         return dividend/divisor;
     }
 }
+
+// class Solution {
+//     public int divide(int dividend, int divisor) {
+//         if(dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+
+//         long x = Math.abs((long)dividend);
+//         long y = Math.abs((long)divisor);
+//         int ans = 0;
+
+//         while(x >= y){
+//             long temp = y;
+//             int multi = 1;
+
+//             while(x >= (temp << 1)){
+//                 temp <<= 1;
+//                 multi <<= 1;
+//             }
+//             x -= temp;
+//             ans += multi;
+//         }
+//         return (dividend > 0) == (divisor > 0) ? ans : -ans;
+//     }
+// }
