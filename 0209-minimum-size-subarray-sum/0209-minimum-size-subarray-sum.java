d93e67d25551889296cdc92b26ba8fb1ae680a4a
MUKUND KUMAR <display.sharif@gmail.com>
2025-10-13 10:34:32 +0530

    Time: 1 ms (99.38%), Space: 58 MB (69.15%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -3,13 +3,13 @@ class Solution {
         int left = 0;
         int right = 0;
         int currSum = 0;
-        int minLength = Integer. MAX_VALUE;
+        int minLength = Integer.MAX_VALUE;
 
         for(right = 0; right < nums.length; right++){
             currSum += nums[right];
             while(currSum >= target){
-                if(right-left+1 < minLength){
-                    minLength = right-left+1;
+                if(right - left + 1 < minLength){
+                    minLength = right - left + 1;
                 }
                 currSum -= nums[left];
                 left++;
@@ -18,3 +18,5 @@ class Solution {
         return minLength != Integer.MAX_VALUE ? minLength : 0;
     }
 }
+
+// j - i + 1 => Size of current subarray
